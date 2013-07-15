18b5da98a7e1005945f77ff6952ad26ec8c111be
oklaova <oklaova@gmail.com>
2013-07-15 15:58:22 +0000

    updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+//var content = fs.readFileSync('index.html','utf-8');
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // read content of the file index.html. The content returned
+  // is a buffer unless the encoding is included (string returned)
+  var content = fs.readFileSync('index.html');
+  response.send(content.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
